c41b7dde8697a8ded74adb6fbdc3b017c447b2da
sofu0185 <sofu0185@edu.easj.dk>
2019-02-01 13:17:53 +0100

    commit 2

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    {
+        public string KlasseNavn { get; set; }
+        public List<Studerende> Klasseliste { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public KlasseRum()
+        {
+            
+        }
+    }
+}
