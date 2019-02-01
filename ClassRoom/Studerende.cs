ab1bda117a43e6261e9f58b475070250de2d38b4
sofu0185 <sofu0185@edu.easj.dk>
2019-02-01 13:15:11 +0100

    commit 1

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        public string Navn { get; set; }
+        public int Fødselsmåned { get; set; }
+        public int Fødselsdag { get; set; }
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            Navn = navn;
+            Fødselsmåned = fødselsmåned;
+            Fødselsdag = fødselsdag;
+        }
+    }
+}
