2c4c9fe1512c08423076834f9f6df9fba8c89692
sofu0185 <sofu0185@edu.easj.dk>
2019-02-01 17:54:00 +0100

    commit 5

---
+++ ClassRoom/Årstid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public static class Årstid
+    {
+        public static string GetÅrstid(int month)
+        {
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Vinter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Forår";
+                case 6:
+                case 7:
+                case 8:
+                    return "Sommer";
+                case 9:
+                case 10:
+                case 11:
+                    return "Efterår";
+                default:
+                    return null;
+            }
+        }
+    }
+}
